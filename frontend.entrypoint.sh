adf798cf91b7ab8dacf47505a7614754006ba9b4
T. Andrew Manning <manninga@illinois.edu>
2020-05-31 09:56:34 -0500

    WIP: Most of form elements in place

--- frontend.entrypoint.sh
+++ frontend.entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cp package.tpl.json package.json
-sed -i "s#{{SCRIPTS_BUILD}}#polymer build --base-path=${WEB_ROOT_PATH} --name=${WEB_ROOT_PATH} --preset=es5-bundled \&\& gulp prpl-server#" package.json
+# cp package.tpl.json package.json
+# sed -i "s#{{SCRIPTS_BUILD}}#polymer build --base-path=${WEB_ROOT_PATH} --name=${WEB_ROOT_PATH} --preset=es5-bundled \&\& gulp prpl-server#" package.json
+export WEB_ROOT_PATH
 cp src/components/des-config.js.tpl src/components/des-config.js
 sed -i "s#{{BACKEND_BASE_URL}}#${BACKEND_BASE_URL}#g" src/components/des-config.js
 sed -i "s#{{API_ROOT_PATH}}#${API_ROOT_PATH}#g" src/components/des-config.js
--- src/components/des-pages/des-cutout.js
+++ src/components/des-pages/des-cutout.js
@@ -12,6 +12,9 @@ import '@polymer/paper-tabs/paper-tab.js';
 import '@polymer/paper-checkbox/paper-checkbox.js';
 import '@polymer/paper-card/paper-card.js';
 import '@polymer/iron-autogrow-textarea/iron-autogrow-textarea.js';
+import '@polymer/paper-radio-group/paper-radio-group.js';
+import '@polymer/paper-radio-button/paper-radio-button.js';
+import '@polymer/paper-slider/paper-slider.js';
 import { SharedStyles } from '../styles/shared-styles.js';
 import {config} from '../des-config.js';
 import { store } from '../../store.js';
@@ -26,6 +29,14 @@ class DESCutout extends connect(store)(PageViewElement) {
       msg: {type: String},
       tabIdx: { type: Number },
       csvFile: {type: String},
+      positions: {type: String},
+      rgb_bands: {type: Object},
+      fits_bands: {type: Object},
+      rgb_types_stiff: {type: Boolean},
+      rgb_types_lupton: {type: Boolean},
+      fits_all_toggle: {type: Boolean},
+      fits: {type: Boolean},
+      submit_disabled: {type: Boolean}
     };
   }
 
@@ -141,9 +152,6 @@ class DESCutout extends connect(store)(PageViewElement) {
               @apply(--layout-center-justified);
           }
 
-          #csv-file-msg {
-            color: red;
-          }
         `,
     ];
   }
@@ -154,34 +162,259 @@ class DESCutout extends connect(store)(PageViewElement) {
     this.username = '';
     this.query = '';
     this.msg = "";
+    this.positions = "";
     this.tabIdx = 0;
     this.csvFile = '';
+    this.rgb_bands = {
+      "checked": {
+        "g": false,
+        "r": false,
+        "i": false,
+        "z": false,
+        "y": false
+      },
+      "disabled": {
+        "g": true,
+        "r": true,
+        "i": true,
+        "z": true,
+        "y": true
+      }
+    };
+    this.fits_bands = {
+      "checked": {
+        "g": false,
+        "r": false,
+        "i": false,
+        "z": false,
+        "y": false
+      }
+    };
+
+    this.fits = false;
+    this.rgb_types_stiff = false;
+    this.rgb_types_lupton = false;
+    this.fits_all_toggle = false;
+    this.submit_disabled = true;
   }
 
   render() {
     return html`
 
     <section>
+        <h2>Positions and data set</h2>
+        <div>
+          <label id="data-release-tag">Data release tag:</label>
+          <paper-radio-group selected="Y6A1" aria-labelledby="data-release-tag">
+            <paper-radio-button name="Y6A1">Y6A1</paper-radio-button>
+            <paper-radio-button name="Y3A2">Y3A2</paper-radio-button>
+            <paper-radio-button name="Y1A1">Y1A1</paper-radio-button>
+            <paper-radio-button name="SVA1">SVA1</paper-radio-button>
+          </paper-radio-group>
+        </div>
+        <div>
+          <paper-tabs id="tab-selector" selected="${this.tabIdx}" @click="${this._updateTabbedContent}">
+            <paper-tab>by COADD ID</paper-tab>
+            <paper-tab>by RA/DEC coordinates</paper-tab>
+          </paper-tabs>
 
-        <paper-tabs id="tab-selector" selected="${this.tabIdx}" @click="${this._updateTabbedContent}">
-          <paper-tab>by COADD ID</paper-tab>
-          <paper-tab>by RA/DEC coordinates</paper-tab>
-        </paper-tabs>
+          <iron-pages selected="${this.tabIdx}">
+              <div>
+                  <iron-autogrow-textarea style="font-family: monospace;" id="coadd-id-textarea" max-rows="10" rows=4 placeholder="COADD_OBJECT_ID\n61407318\n61407582" value=""></iron-autogrow-textarea>
+              </div>
+              <div>
+                  <iron-autogrow-textarea style="font-family: monospace;" id="coords-textarea" max-rows="10" rows=4 placeholder="RA,DEC\n21.5,3.48\n36.6,-15.68" value=""></iron-autogrow-textarea>
+              </div>
+          </iron-pages>
+        </div>
+        <div>
+          <div>
+            &nbsp;&nbsp;
+            <paper-button raised class="indigo" id="bc_uploadFile">
+              <span style="overflow-x: auto; overflow-wrap: break-word;">Upload CSV file</span>
+              <input type="file" class="upload" id="csv-upload" @change="${e => this._fileChange(e)}" accept=".csv, .CSV" />
+            </paper-button>
+          </div>
+          <div id="csv-file-msg"></div>
+        </div>
+    </section>
 
-        <iron-pages selected="${this.tabIdx}">
-            <div>
-                <iron-autogrow-textarea id="coadd-id-textarea" max-rows="15" rows=12 placeholder="COADD_OBJECT_ID\n61407318\n61407582" value=""></iron-autogrow-textarea>
-            </div>
-            <div>
-                <iron-autogrow-textarea id="coords-textarea" max-rows="15" rows=12 placeholder="RA,DEC\n21.5,3.48\n36.6,-15.68" value=""></iron-autogrow-textarea>
-            </div>
-        </iron-pages>
+    <section>
+        <h2>Output format and size</h2>
+        <p>Check all desired output files. FITS format requires selection of one or more bands. RGB color images require selection of exactly three bands.</p>
+        <h3>FITS format</h3>
+        <div style="float: none;">
+        <p>FITS format requires selection of one or more bands.</p>
+        <paper-checkbox @change="${e => this._updateFitsBands(e)}" ?checked="${this.fits}">FITS (FITS format)</paper-checkbox>&nbsp;&nbsp;&nbsp;&nbsp;
+        <paper-checkbox @change="${e => this._selectAllFitsBands(e)}" ?checked="${this.fits_all_toggle}" ?disabled="${!this.fits}" style="font-weight: bold;" id="bc_all_toggle">Select All/None</paper-checkbox>&nbsp;
+        </div>
+        <div style="float: none;">
+          &nbsp;
+          &nbsp;
+          <paper-checkbox @change="${e => this._updateFitsBandsSelection(e, 'g')}" ?checked="${this.fits_bands.checked.g}" ?disabled="${!this.fits}" class="bandbox" style="font-size:16px; padding-top:15px;" id="bc_gband">g</paper-checkbox>&nbsp;&nbsp;
+          <paper-checkbox @change="${e => this._updateFitsBandsSelection(e, 'r')}" ?checked="${this.fits_bands.checked.r}" ?disabled="${!this.fits}" class="bandbox" style="font-size:16px; padding-top:15px;" id="bc_rband">r</paper-checkbox>&nbsp;&nbsp;
+          <paper-checkbox @change="${e => this._updateFitsBandsSelection(e, 'i')}" ?checked="${this.fits_bands.checked.i}" ?disabled="${!this.fits}" class="bandbox" style="font-size:16px; padding-top:15px;" id="bc_iband">i</paper-checkbox>&nbsp;&nbsp;
+          <paper-checkbox @change="${e => this._updateFitsBandsSelection(e, 'z')}" ?checked="${this.fits_bands.checked.z}" ?disabled="${!this.fits}" class="bandbox" style="font-size:16px; padding-top:15px;" id="bc_zband">z</paper-checkbox>&nbsp;&nbsp;
+          <paper-checkbox @change="${e => this._updateFitsBandsSelection(e, 'y')}" ?checked="${this.fits_bands.checked.y}" ?disabled="${!this.fits}" class="bandbox" style="font-size:16px; padding-top:15px;" id="bc_Yband">Y</paper-checkbox>&nbsp;&nbsp;
 
-        <input type="file" id="csv-upload" @change="${e => this._fileChange(e)}" accept=".csv, .CSV" />
-        <div id="csv-file-msg"></div>
+        </div>
+        <h3>Color image format</h3>
+        <p>RGB color images require selection of exactly three bands.</p>
+        <div>
+          <paper-checkbox @change="${e => this._updateRgbTypes(e, 'stiff')}" ?checked="${this.rgb_types_stiff}">Color image (RGB: STIFF format)</paper-checkbox>
+        </div>
+        <div>
+          <paper-checkbox @change="${e => this._updateRgbTypes(e, 'lupton')}" ?checked="${this.rgb_types_lupton}">Color image (RGB: Lupton method)</paper-checkbox>
+        </div>
 
+        <div style="float: none;">
+          &nbsp;
+          &nbsp;
+          <paper-checkbox @change="${e => this._updateRgbSelection(e, 'g')}" ?checked="${this.rgb_bands.checked.g}" ?disabled="${this.rgb_bands.disabled.g}" class="bandbox" style="font-size:16px; padding-top:15px;" id="bc_gband">g</paper-checkbox>&nbsp;&nbsp;
+          <paper-checkbox @change="${e => this._updateRgbSelection(e, 'r')}" ?checked="${this.rgb_bands.checked.r}" ?disabled="${this.rgb_bands.disabled.r}" class="bandbox" style="font-size:16px; padding-top:15px;" id="bc_rband">r</paper-checkbox>&nbsp;&nbsp;
+          <paper-checkbox @change="${e => this._updateRgbSelection(e, 'i')}" ?checked="${this.rgb_bands.checked.i}" ?disabled="${this.rgb_bands.disabled.i}" class="bandbox" style="font-size:16px; padding-top:15px;" id="bc_iband">i</paper-checkbox>&nbsp;&nbsp;
+          <paper-checkbox @change="${e => this._updateRgbSelection(e, 'z')}" ?checked="${this.rgb_bands.checked.z}" ?disabled="${this.rgb_bands.disabled.z}" class="bandbox" style="font-size:16px; padding-top:15px;" id="bc_zband">z</paper-checkbox>&nbsp;&nbsp;
+          <paper-checkbox @change="${e => this._updateRgbSelection(e, 'y')}" ?checked="${this.rgb_bands.checked.y}" ?disabled="${this.rgb_bands.disabled.y}" class="bandbox" style="font-size:16px; padding-top:15px;" id="bc_Yband">Y</paper-checkbox>&nbsp;&nbsp;
+        </div>
+        <h3>Cutout size (arcminutes)</h3>
+        <div>
+          X &nbsp;<paper-slider id="bc_xsizeSlider" pin min="0.1" max="12" max-markers="10" step="0.1" value="1.0" expand editable></paper-slider>
+          Y &nbsp;<paper-slider id="bc_ysizeSlider" pin min="0.1" max="12" max-markers="10" step="0.1" value="1.0" expand editable></paper-slider>
+        </div>
+    </section>
+    <section>
+        <h2>Options</h2>
+        <div>
+          <p>Provide a custom job name:</p>
+          <paper-input always-float-label id="bc_validname" name="name" label="Job Name" value="" style = "max-width: 500px; padding-left:2rem;"></paper-input>
+          <p>Receive an email when the files are ready for download:</p>
+          <paper-checkbox style="font-size:16px; padding-left:2rem; padding-top:15px;" id="bc_send_email">Email when complete</paper-checkbox>&nbsp;&nbsp;
+        </div>
+    </section>
+    <section>
+    <div>
+      <paper-button raised  class="indigo"  id="bc_submitJobButton" ?disabled="${this.submit_disabled}" on-tap="_submitJob">
+        <i class="fa fa-cogs"></i> &nbsp;  &nbsp;Submit Job
+      </paper-button>
+    </div>
     </section>
     `;
+  }
+
+  _validateForm() {
+    var validForm = true;
+    validForm = this.positions !== "" && validForm;
+    validForm = (this.fits || this.rgb_types_stiff || this.rgb_types_lupton) && validForm;
+    if (this.fits) {
+      validForm = this._bandsSelected(this.fits_bands) > 0 && validForm;
+    }
+    if (this.rgb_types_stiff || this.rgb_types_lupton) {
+      validForm = this._bandsSelected(this.rgb_bands) === 3 && validForm;
+    }
+    // Enable/disable submit button
+    this.submit_disabled = !validForm;
+  }
+  _bandsSelected(obj) {
+    var bandsSelected = 0;
+    for (var b in obj.checked) {
+      if (obj.checked[b]) {
+        bandsSelected += 1;
+      }
+    }
+    return bandsSelected
+  }
+  _updateRgbTypes(event, type) {
+    if (type === 'stiff') {
+      this.rgb_types_stiff = event.target.checked;
+    } else {
+      this.rgb_types_lupton = event.target.checked;
+    }
+    if (this.rgb_types_lupton === false && this.rgb_types_stiff === false) {
+      for (var b in this.rgb_bands.disabled) {
+        this.rgb_bands.disabled[b] = true;
+      }
+    } else {
+      for (var b in this.rgb_bands.disabled) {
+        this.rgb_bands.disabled[b] = false;
+      }
+    }
+  }
+
+  _selectAllFitsBands(event) {
+    for (var b in this.fits_bands.checked) {
+      this.fits_bands.checked[b] = event.target.checked;
+    }
+    this.fits_all_toggle = event.target.checked;
+    // This hack seems necessary to trigger the hasChanged() function for the property
+    var temp = this.fits_bands;
+    this.fits_bands =  {};
+    this.fits_bands =  temp;
+  }
+
+  _updateFitsBands(event) {
+    this.fits = event.target.checked;
+    for (var b in this.fits_bands.disabled) {
+      this.fits_bands.disabled[b] = !this.fits;
+    }
+    // This hack seems necessary to trigger the hasChanged() function for the property
+    var temp = this.fits_bands;
+    this.fits_bands =  {};
+    this.fits_bands =  temp;
+
+  }
+
+  _updateFitsBandsSelection(event, band) {
+    this.fits_bands.checked[band] = event.target.checked;
+    console.log('fits toggle: ' + this.fits_all_toggle);
+    if (this.fits_bands.checked[band] === false) {
+      console.log('unchecked a band');
+      this.fits_all_toggle = false;
+    } else {
+      var allChecked = true;
+      for (var b in this.fits_bands.checked) {
+        allChecked = allChecked && this.fits_bands.checked[b];
+      }
+      this.fits_all_toggle = allChecked;
+    }
+    //This hack seems necessary to trigger the hasChanged() function for the property
+    var temp = this.fits_bands;
+    this.fits_bands =  {};
+    this.fits_bands =  temp;
+    console.log(JSON.stringify(this.fits_bands));
+
+  }
+
+  _updateRgbSelection(event, band) {
+    // event.target.disabled = true
+    // console.log('checked: '+ event.target.checked)
+    // console.log('disabled: '+ event.target.disabled)
+    // console.log(band)
+    this.rgb_bands.checked[band] = event.target.checked;
+    var numChecked = 0;
+    for (var b in this.rgb_bands.checked) {
+      if (this.rgb_bands.checked[b]) {
+        numChecked += 1;
+      }
+    }
+    // console.log('numChecked = ' + numChecked);
+    if (numChecked > 2) {
+      for (var b in this.rgb_bands.disabled) {
+        if (!this.rgb_bands.checked[b]) {
+          console.log('disabling ' + b);
+          this.rgb_bands.disabled[b] = true;
+        }
+      }
+    } else {
+      for (var b in this.rgb_bands.disabled) {
+        this.rgb_bands.disabled[b] = false;
+      }
+    }
+    // This hack seems necessary to trigger the hasChanged() function for the property
+    var temp = this.rgb_bands;
+    this.rgb_bands =  {};
+    this.rgb_bands =  temp;
+
+    console.log(JSON.stringify(this.rgb_bands))
   }
 
   _fileChange(event) {
@@ -219,9 +452,12 @@ class DESCutout extends connect(store)(PageViewElement) {
           that.tabIdx = 0
         }
         that.shadowRoot.getElementById(textareaID).value = data.csv;
-          that.shadowRoot.getElementById('csv-file-msg').innerHTML = '';
+        that.positions = data.csv;
+        that.shadowRoot.getElementById('csv-file-msg').innerHTML = 'Positions validated and processed.';
+        that.shadowRoot.getElementById('csv-file-msg').style.color = 'green';
       } else {
         that.shadowRoot.getElementById('csv-file-msg').innerHTML = data.msg;
+        that.shadowRoot.getElementById('csv-file-msg').style.color = 'red';
       }
     })
   }
@@ -233,6 +469,16 @@ class DESCutout extends connect(store)(PageViewElement) {
     this.tabIdx = this.shadowRoot.getElementById('tab-selector').selected;
   }
 
+  // firstUpdated() {
+  //   console.log('First Updated: '+ JSON.stringify(this.rgb_bands.checked));
+  // }
+
+  updated(changedProps) {
+    changedProps.forEach((oldValue, propName) => {
+      console.log(`${propName} changed. oldValue: ${oldValue}`);
+    });
+    this._validateForm();
+  }
 }
 
 window.customElements.define('des-cutout', DESCutout);
