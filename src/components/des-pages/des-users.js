f59cb2cd42ffcbd5151ff2a8b01fd8de05689703
T. Andrew Manning <manninga@illinois.edu>
2020-07-09 10:25:07 -0500

    User roles can be modified in user management app

--- src/components/des-pages/des-users.js
+++ src/components/des-pages/des-users.js
@@ -1,4 +1,5 @@
 import { html,css } from 'lit-element';
+import { render } from 'lit-html';
 import { PageViewElement } from './des-base-page.js';
 import { SharedStyles } from '../styles/shared-styles.js';
 import { config } from '../des-config.js';
@@ -38,9 +39,11 @@ class DESUsers extends connect(store)(PageViewElement) {
         <vaadin-grid .multiSort="${true}" style="height: 70vh;">
           <vaadin-grid-selection-column auto-select></vaadin-grid-selection-column>
           <vaadin-grid-column path="user.name" .renderer="${this._rendererJobId}"   header="Username">  </vaadin-grid-column>
+          <vaadin-grid-column auto-width flex-grow="0" text-align="center" .renderer="${this.rendererAction}" .headerRenderer="${this._headerRendererAction}"></vaadin-grid-column>
           <vaadin-grid-column path="user.roles" .renderer="${this._rendererJobName}" header="Roles"></vaadin-grid-column>
         </vaadin-grid>
       </section>
+      <vaadin-dialog id="edit-user-dialog"></vaadin-dialog>
     `;
   }
 
@@ -50,9 +53,174 @@ class DESUsers extends connect(store)(PageViewElement) {
 
   firstUpdated() {
     this.grid = this.shadowRoot.querySelector('vaadin-grid');
+    this.rendererAction = this._rendererAction.bind(this); // need this to invoke class methods in renderers
+    this.userEditDialog = this._userEditDialog.bind(this); // need this to invoke class methods in renderers
     this._fetchUserList();
   }
 
+  _headerRendererAction(root) {
+    render(
+      html`
+        <a title="Actions"><iron-icon icon="vaadin:cogs"></iron-icon></a>
+      `,
+      root
+    );
+  }
+
+  _getRolesForUser(username) {
+    let roles = [];
+    for (let i in this.grid.items) {
+      if (username === this.grid.items[i].user.name) {
+        roles = this.grid.items[i].user.roles
+        break;
+      }
+    }
+    return roles;
+  }
+
+  _rendererAction(root, column, rowData) {
+    let container = root.firstElementChild;
+    if (!container) {
+      container = root.appendChild(document.createElement('div'));
+    }
+    let selected = this.grid.selectedItems;
+    if (selected.length === 0) {
+      render(
+        html`
+          <a title="Edit user ${rowData.item.user.name}..." onclick="return false;"><iron-icon @click="${(e) => {this.userEditDialog(rowData.item.user);}}" icon="vaadin:pencil" style="color: darkgray;"></iron-icon></a>
+        `,
+        container
+      );
+    } else {
+      if (selected.map((e) => {return e.user.name}).indexOf(rowData.item.user.name) > -1) {
+        render(
+          html`
+            <a title="Edit (${selected.length}) Selected Users" onclick="return false;"><iron-icon @click="${(e) => {this.userEditDialog(rowData.item.user);}}" icon="vaadin:pencil" style="color: red;"></iron-icon></a>
+          `,
+          container
+        );
+      } else {
+        render(
+          html``,
+          container
+        );
+
+      }
+    }
+  }
+
+  _userEditDialog(userInfo) {
+    const editUserDialog = this.shadowRoot.getElementById('edit-user-dialog');
+    editUserDialog.renderer = (root, dialog) => {
+      let container = root.firstElementChild;
+      if (!container) {
+        container = root.appendChild(document.createElement('div'));
+      }
+      // Different behavior if applying to selection of users
+      let placeholderText = userInfo.roles.join(', ');
+      let classList = '';
+      let headerText = `Update user: ${userInfo.name}`;
+      if (this.grid.selectedItems.length > 0) {
+        placeholderText = 'CAUTION: Roles will apply to ALL users!!';
+        classList = 'warning-text';
+        headerText = `Updating ALL SELECTED user roles! [not yet supported]`;
+      }
+      render(
+        html`
+          <style>
+            paper-button {
+              width: 100px;
+              text-transform: none;
+              --paper-button-raised-keyboard-focus: {
+                background-color: var(--paper-indigo-a250) !important;
+                color: white !important;
+              };
+            }
+            paper-button.indigo {
+              background-color: var(--paper-indigo-500);
+              color: white;
+              width: 100px;
+              text-transform: none;
+              --paper-button-raised-keyboard-focus: {
+                background-color: var(--paper-indigo-a250) !important;
+                color: white !important;
+              };
+            }
+            paper-button.des-button {
+                background-color: white;
+                color: black;
+                width: 100px;
+                text-transform: none;
+                --paper-button-raised-keyboard-focus: {
+                  background-color: white !important;
+                  color: black !important;
+                };
+            }
+           .warning-text {
+             color: red;
+           }
+          </style>
+          <div style="width: 50vw">
+            <a title="Close" href="#" onclick="return false;">
+              <iron-icon @click="${(e) => {dialog.opened = false;}}" icon="vaadin:close" style="position: absolute; top: 2rem; right: 2rem; color: darkgray;"></iron-icon>
+            </a>
+            <h3 class="${classList}">${headerText}</h3>
+            <paper-input id="roles-input" always-float-label label="Update assigned roles" class="${classList}" value="${placeholderText}" placeholder="${placeholderText}"></paper-input>
+
+            <paper-button @click="${(e) => {dialog.opened = false; this._setUserRoles(userInfo, document.getElementById('roles-input').value);}}" class="des-button" raised>Apply</paper-button>
+            <paper-button @click="${(e) => {dialog.opened = false;}}" class="indigo" raised>Cancel</paper-button>
+          </div>
+        `,
+        container
+      );
+    }
+    editUserDialog.opened = true;
+  }
+
+  _setUserRoles(userInfo, newRolesStr) {
+    // TODO: Support bulk role update for multiple selected users
+    if (this.grid.selectedItems.length > 0) {
+      return;
+    }
+    let roles = [];
+    // Convert input CSV string to array, trimming surrounding whitespace and replacing remaining whitespace with underscores
+    let newRoles = newRolesStr.split(',');
+    for (let i in newRoles) {
+      let newRole = newRoles[i].trim().replace(/\s/g, "_");
+      if (newRole !== '') {
+        roles.push(newRole);
+      }
+    }
+    newRoles = roles;
+    if (newRoles === userInfo.roles) {
+      return;
+    }
+    const Url=config.backEndUrl + "user/update/roles"
+    let body = {
+      'username': userInfo.name,
+      'new_roles': newRoles
+    };
+    const param = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem("token")
+      },
+      body: JSON.stringify(body)
+    };
+    fetch(Url, param)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      if (data.status === "ok") {
+        // console.log(JSON.stringify(data.users, null, 2));
+        this._fetchUserList();
+      } else {
+        console.log(JSON.stringify(data, null, 2));
+      }
+    });
+  }
 
   _fetchUserList() {
     const Url=config.backEndUrl + "page/users/list"
@@ -65,15 +233,14 @@ class DESUsers extends connect(store)(PageViewElement) {
       },
       body: JSON.stringify(body)
     };
-    var that = this;
     fetch(Url, param)
     .then(response => {
       return response.json()
     })
     .then(data => {
       if (data.status === "ok") {
-        console.log(JSON.stringify(data.users, null, 2));
-        that._updateUserTable(data.users);
+        // console.log(JSON.stringify(data.users, null, 2));
+        this._updateUserTable(data.users);
       } else {
         console.log(JSON.stringify(data, null, 2));
       }
