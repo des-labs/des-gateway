2864528aeb9612c49d5fcb8dd6dfaac6f952f2c8
T. Andrew Manning <manninga@illinois.edu>
2020-10-03 14:56:33 -0500

    File lists are arrays not JSON-formatted strings now

--- src/components/des-pages/des-job-status.js
+++ src/components/des-pages/des-job-status.js
@@ -974,11 +974,12 @@ class DESJobStatus extends connect(store)(PageViewElement) {
       } 
       job.data = typeof(item.data) === 'string' ? JSON.parse(item.data) : null;
       job.query = item.query;
-      job.query_files = typeof(item.query_files) === 'string' ? item.query_files : null;
-      job.cutout_files = typeof(item.cutout_files) === 'string' ? item.cutout_files : null;
+      job.query_files = typeof(item.query_files) === 'object' ? item.query_files : null;
+      job.cutout_files = typeof(item.cutout_files) === 'object' ? item.cutout_files : null;
       job.cutout_positions = typeof(item.cutout_positions) === 'string' ? item.cutout_positions : null;
       job.renewal_token = typeof(item.renewal_token) === 'string' ? item.renewal_token : null;
       job.expiration_date = typeof(item.expiration_date) === 'string' ? item.expiration_date : null;
+      // console.log(JSON.stringify(job, null, 2));
       if (job.type !== 'query' || job.data === null) {
         gridItems.push({job: job});
       }
