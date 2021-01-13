877a3ac627e22ed866868e51db4ae66f89606664
Michael D. Johnson <mdjohnson44@gmail.com>
2021-01-13 15:59:48 -0600

    updating query for dr2 galaxy/healpix query

--- images/example-query-4-public.sql
+++ images/example-query-4-public.sql
@@ -1,15 +1,11 @@
 --
 -- Example Query --
--- This query creates a Helpix map of number of galaxies
+-- This query creates a Healpix map of number of high confidence galaxies
 -- and their mean magnitude on a resolution of NSIDE = 1024
--- using NEST Schema
+-- using NEST Schema and a sample of our full DR2 database table
 SELECT count(dr2.MAG_AUTO_I) COUNT,avg(dr2.MAG_AUTO_I) AVERAGE,dr2.HPIX_1024
-FROM DR2_MAIN dr2
+FROM DR2_MAIN_SAMPLE dr2
 WHERE
-  dr2.WAVG_SPREAD_MODEL_I + 3.0*dr2.WAVG_SPREADERR_MODEL_I > 0.005 and
-  dr2.WAVG_SPREAD_MODEL_I + 1.0*dr2.WAVG_SPREADERR_MODEL_I > 0.003 and
-  dr2.WAVG_SPREAD_MODEL_I - 1.0*dr2.WAVG_SPREADERR_MODEL_I > 0.001 and
-  dr2.WAVG_SPREAD_MODEL_I > -1 and
-  dr2.IMAFLAGS_ISO_I = 0 and
-  dr2.MAG_AUTO_I < 23
+  dr2.MAG_AUTO_I < 23 AND
+  dr2.EXTENDED_CLASS_COADD=3
 GROUP BY dr2.HPIX_1024
