c28be3cf94785501b54feabbf0a3f50661d5feb4
Michael D. Johnson <mdjohnson44@gmail.com>
2021-01-13 16:05:09 -0600

    updating stellar healpix query sample to use sample dr2 table

--- images/example-query-3-public.sql
+++ images/example-query-3-public.sql
@@ -2,11 +2,11 @@
 -- Example Query --
 -- This query creates a Healpix map of number of stars
 -- and their mean magnitude on a resolution of NSIDE = 1024
--- using NEST Schema
+-- using NEST Schema and a sample of our full DR2 database table
 SELECT
 count(main.MAG_AUTO_I) COUNT,
 main.HPIX_1024
-FROM DR2_MAIN main
+FROM DR2_MAIN_SAMPLE main
 WHERE
   main.WAVG_SPREAD_MODEL_I + 3.0*main.WAVG_SPREADERR_MODEL_I < 0.005 and
   main.WAVG_SPREAD_MODEL_I > -1 and
